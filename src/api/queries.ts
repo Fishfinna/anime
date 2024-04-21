c278dc59e06e3a6f2c2164d7827f119ebd2f09b6
Shanti <fishfinna12@gmail.com>
2024-04-20 22:06:10 -0700

    fixed the screen cut

--- src/api/queries.ts
+++ src/api/queries.ts
@@ -34,7 +34,7 @@ export function titlesQuery(query: string, page: number = 0) {
     `,    
     variables:  {
         search: { allowAdult: false, allowUnknown: false, query},
-        limit: 4,
+        limit: 6,
         page: page + 1,
         countryOrigin: "ALL",
     }
