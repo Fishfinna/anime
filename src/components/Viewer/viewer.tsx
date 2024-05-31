57d5110b6d50cc6dd43c560085d0f4f1ca955bc3
Shanti <fishfinna12@gmail.com>
2024-05-30 19:32:10 -0700

    reload will also update the page content

--- src/components/Viewer/viewer.tsx
+++ src/components/Viewer/viewer.tsx
@@ -1,4 +1,11 @@
-import { For, Show, createEffect, createSignal, useContext } from "solid-js";
+import {
+  For,
+  Show,
+  createEffect,
+  createSignal,
+  onMount,
+  useContext,
+} from "solid-js";
 import { Mode } from "../../types/settings";
 import { SettingsContext } from "../../context/settingsContext";
 import { Toggle } from "../Toggle/toggle";
@@ -34,40 +41,6 @@ export function Viewer(param: { showId?: string }) {
   const [isLoading, setIsLoading] = createSignal<boolean>(false);
   const [urls, setUrls] = createSignal<url[]>([]);
   const dateOffset = 1000;
-
-  // use the show id for the page
-  createEffect(async () => {
-    if (param.showId) {
-      setMode(Mode.episode);
-      setIsLoading(true);
-      try {
-        const showId = param.showId;
-        const { query, variables } = getShow(showId);
-        const { data: response } = await client
-          .query(query, variables)
-          .toPromise();
-        const showData = response.show;
-        if (!showData) {
-          throw new Error("No search results.");
-        }
-        setCurrentTitle(showData);
-        setUrls(
-          await getUrls({
-            showId: currentTitle()?._id || "",
-            episodeString: episodeNumber() || "1",
-            translationType: lang(),
-          })
-        );
-      } catch (err: any) {
-        setError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    if (!currentTitle()) {
-      setMode(Mode.none);
-    }
-  }, []);
 
   // handle date modified
   createEffect(() => {
@@ -130,6 +103,46 @@ export function Viewer(param: { showId?: string }) {
       setIsLoading(false);
     }
   }, [episodeNumber, isDub, lang]);
+
+  // reload the content when the page reloads
+  onMount(async () => {
+    if (param.showId || currentTitle()?._id) {
+      let showId: string;
+      if (param.showId) {
+        showId = param.showId;
+      } else {
+        showId = currentTitle()?._id as string;
+      }
+      setMode(Mode.episode);
+      setIsLoading(true);
+      try {
+        const { query, variables } = getShow(showId);
+        const { data: response } = await client
+          .query(query, variables)
+          .toPromise();
+        const showData = response.show;
+        if (!showData) {
+          throw new Error("No search results.");
+        }
+        setCurrentTitle(showData);
+        setUrls(
+          await getUrls({
+            showId: currentTitle()?._id || "",
+            episodeString: episodeNumber() || "1",
+            translationType: lang(),
+          })
+        );
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (!currentTitle()) {
+      setMode(Mode.none);
+    }
+  });
 
   return (
     <Show when={mode() === Mode.episode}>
