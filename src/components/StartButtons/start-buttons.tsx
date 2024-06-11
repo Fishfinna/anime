0401471c342dd81520b92a5dd337f523c3cc7fc5
Shanti <fishfinna12@gmail.com>
2024-06-10 20:01:32 -0700

    lint fixes

--- src/components/StartButtons/start-buttons.tsx
+++ src/components/StartButtons/start-buttons.tsx
@@ -4,7 +4,7 @@ import { useContext } from "solid-js";
 import { Mode, SearchType } from "../../types/settings";
 
 export function StartButtons() {
-  const { setMode, mode, setSearchType } = useContext(SettingsContext);
+  const { setMode, setSearchType } = useContext(SettingsContext);
 
   function findPopular(event: Event) {
     event.preventDefault();
@@ -18,11 +18,11 @@ export function StartButtons() {
     setMode(Mode.title);
   }
 
-  function findRandom(event: Event) {
-    event.preventDefault();
-    setSearchType(SearchType.random);
-    setMode(Mode.title);
-  }
+  // function findRandom(event: Event) {
+  //   event.preventDefault();
+  //   setSearchType(SearchType.random);
+  //   setMode(Mode.title);
+  // }
 
   return (
     <div class="start-buttons">
