019c6a2575dccea5f94c630c88fbfdcb853edd25
Shanti <fishfinna12@gmail.com>
2025-05-09 11:25:59 -0700

    started on the title componenet refector

--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -1,3 +1,39 @@
+import { useContext } from "solid-js";
 import "./title.scss";
+import { useNavigate } from "@solidjs/router";
+import { SettingsContext } from "../../context/settingsContext";
+import { Mode } from "../../types/settings";
+import { Icon } from "../Icons/icon";
+import { Title as TitleType } from "../../types/titles";
 
-export function Title() {}
+export function Title(title: TitleType) {
+  const { setMode, setCurrentTitle } = useContext(SettingsContext);
+  const navigate = useNavigate();
+
+  <div
+    class="title"
+    onClick={(event) => {
+      event.preventDefault();
+      setCurrentTitle(title);
+      setMode(Mode.episode);
+      navigate(`/anime?show_id=${title._id}`);
+    }}
+  >
+    <img
+      src={title.thumbnail}
+      alt={title.englishName || title.name}
+      onError={(e) => {
+        const img = e.currentTarget;
+        if (!img.dataset.fallback) {
+          img.dataset.fallback = "true";
+          img.src = "/anime/default-thumbnail.png";
+        }
+      }}
+    />
+    <Icon
+      name="play_arrow filled"
+      className="play-arrow material-icons-round"
+    />
+    <h3 class="thumbnail-title">{title.englishName || title.name}</h3>
+  </div>;
+}
